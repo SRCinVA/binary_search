4f4f57577b80c54a2fc2ed3b4bb8f7c90e16be17
SRCinVA <scitarella@hotmail.com>
2021-04-20 17:50:10 -0400

    on to binary search function

--- binary_search.py
+++ binary_search.py
@@ -6,4 +6,16 @@ def naive_search(l,target):
             return i        # return that index spot (easy enough)
     return -1               # if it's not there, return -1; we went through the entire list.
 
-# def binary_search
+
+# you can take advantage of how the list is sorted
+def binary_search(l, target):
+    # example l == [1,3,5,10,12]
+    midpoint = (len(l)) // 2 # divide by 2 and round it down
+    if l[midpoint] == target:
+        return midpoint
+    elif target < l[midpoint]:
+        return binary_search(l,search)  # you recursively chop it in half and try it again
+    else: # meaning, target > l[midpoint]
+        return binary_search(l,search)
+
+
