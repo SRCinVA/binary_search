d4df37498a6502d3000f4c05c3ab2343e28daba6
SRCinVA <scitarella@hotmail.com>
2021-04-21 16:43:32 -0400

    changed to low and high indices

--- binary_search.py
+++ binary_search.py
@@ -6,16 +6,20 @@ def naive_search(l,target):
             return i        # return that index spot (easy enough)
     return -1               # if it's not there, return -1; we went through the entire list.
 
-
 # you can take advantage of how the list is sorted
-def binary_search(l, target):
-    # example l == [1,3,5,10,12]
-    midpoint = (len(l)) // 2 # divide by 2 and round it down
+def binary_search(l, target, low=None, high=None):  # the lowest and highest indices we search
+    # example l == [1,3,5,10,12]                    # note that low and high are enabled terms for lists.
+    if low is None:
+        low = 0  # ... so that it is the lowest possible index we could check
+    if high is None:
+        high = len(l) - 1  # ... here, obviously the highest possible index (that's why it's - 1)
+
+    midpoint = (low + high) // 2 # add up lowest and highest indices and divide by 2
     if l[midpoint] == target:
         return midpoint
     elif target < l[midpoint]:
-        return binary_search(l,search)  # you recursively chop it in half and try it again
+        return binary_search(l, search, low, midpoint-1)  # you recursively chop it in half and try it again
     else: # meaning, target > l[midpoint]
-        return binary_search(l,search)
+        return binary_search(l,search, midpoint+1, high)
 
 
